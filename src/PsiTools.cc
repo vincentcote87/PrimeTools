8d9cbc835172c014d86a2f8822fe09b2c147c0b6
Julius J Moore <julius.moore@uleth.ca>
2018-06-27 16:01:25 -0600

    Began process of changing u from integer to float

--- src/PsiTools.cc
+++ src/PsiTools.cc
@@ -40,7 +40,7 @@ long double psi(uint64_t x) {
 long double psi_work(uint64_t x) {
   if (x < 2)
     return 0.0;
-  long long u = floor(pow(static_cast<long double>(x), (1.0L/3.0L)) * cbrtl(pow(log(log(x)), 2)));
+  long double u = pow(static_cast<long double>(x), (1.0L/3.0L)) * cbrtl(pow(log(log(x)), 2));
   if (u < 1)
     u = 1;
   return S1(x, u) + S2(x, u) - S3(x, u) - slowS4(x, u);
@@ -63,14 +63,15 @@ long double T(long double t) {
   return T1 + T2 + T3;
 }
 
-long double S1(uint64_t x, uint64_t u) {
-  return primetools::calculatePsiLongDouble(static_cast<uint64_t>(u));
+long double S1(const uint64_t x, const long double u) {
+   return primetools::calculatePsiLongDouble(static_cast<uint64_t>(u)); //floor is okay
 }
-long double S2(uint64_t x, uint64_t u) {
+
+long double S2(const uint64_t x, const long double u) {
   long double S = 0.0;
   uint64_t S2b = 0;
 
-  for(uint m = 1; m <= u; m++) {
+  for(uint64_t m = 1; m <= (uint64_t) u; m++) { //floor is okay
     S2b = x/m;
     long double sum = 0.0;
     if(S2b > 100000)
@@ -86,7 +87,7 @@ long double S2(uint64_t x, uint64_t u) {
   return S;
 }
 
-uint64_t pow(uint64_t a, uint64_t b) {
+uint64_t pow(const uint64_t a, const uint64_t b) {
    if (b == 0)
       return 1;
    if (b == 1)
@@ -97,9 +98,9 @@ uint64_t pow(uint64_t a, uint64_t b) {
    return half*half*a;
 }
 
-long long S3_B(const uint64_t x, const uint64_t u, const uint64_t p, const uint64_t l) {
+long long S3_B(const uint64_t x, const long double u, const uint64_t p, const uint64_t l) {
   long long result = 0;
-  const uint64_t L = (uint64_t) (log((long double) u)/log((long double) p)); //intentional floor
+  const uint64_t L = (uint64_t) (log(u)/log((long double) p)); //intentional floor //FLOOR OF U MIGHT BE NOT OK
   for (uint64_t k = 1; k <= L; ++k) {
     result += x / (l * pow( p, k)); //intentional floor
   }
@@ -107,10 +108,10 @@ long long S3_B(const uint64_t x, const uint64_t u, const uint64_t p, const uint6
   return result;
 }
 
-long double S3_A(const uint64_t x, const uint64_t u, const uint64_t p) {
+long double S3_A(const uint64_t x, const long double u, const uint64_t p) {
   long double S = 0.0;
   // uint64_t tmpMu = 0;
-  for (uint64_t l = 1; l <= u; ++l) {
+  for (uint64_t l = 1; l <= (uint64_t) u; ++l) { //floor was okay for less than or equal to u
     S += mobius(l)*S3_B(x, u, p, l);
     // tmpMu += mobius(l);
     // std::cout<<tmpMu<<std::endl;
@@ -118,36 +119,36 @@ long double S3_A(const uint64_t x, const uint64_t u, const uint64_t p) {
   return S;
 }
 
-long double S3(const uint64_t x, const uint64_t u) {
+long double S3(const uint64_t x, const long double u) {
   long double S = 0.0;
 
   primesieve::iterator it;
   it.skipto(0);
   uint64_t prime = it.next_prime();
 
-  for (; prime <= u; prime = it.next_prime()) {
+  for (; prime <= (uint64_t) u; prime = it.next_prime()) { //floor is okay for less than or equal to u
     S += log(prime)*S3_A(x, u, prime);
     // std::cout<<S<<std::endl;
   }
   // std::cout<<u<<std::endl;
   return S;
 }
-long double S4(uint64_t x, uint64_t u) {
+long double S4(const uint64_t x, const long double u) {
   long double psi_of_u = primetools::calculatePsiLongDouble(u);
   return S4a(x, u, psi_of_u) + S4b(x, u, psi_of_u);
 }
 
-long double S4a(uint64_t x, uint64_t u, long double psiOfU) {
+long double S4a(const uint64_t x, const long double u, const long double psiOfU) {
   long double result = 0.0;
-  for(uint64_t l = 1; l <= u; ++l) {
+  for(uint64_t l = 1; l <= (uint64_t) u; ++l) { //floor was okay
     result += mobius(l) * S4a_innerLoop(x, u, l, psiOfU);
   }
   return result;
 }
 
-long double S4a_innerLoop(uint64_t x, uint64_t u, uint64_t l, long double psiOfU) {
+long double S4a_innerLoop(const uint64_t x, const long double u, const uint64_t l, const long double psiOfU) {
   long double result = 0.0;
-  uint64_t lowerM = u/l;
+  uint64_t lowerM = u/((long double) l); //VERY LIKELY WAS A FAILURE POINT WHEN U WAS AN INTEGER
   uint64_t upperM = sqrt(x/l);
   for(uint64_t m = lowerM + 1; m <= upperM; ++m) {
     result += (primetools::calculatePsiLongDouble(x/(l * m)) - psiOfU);
@@ -155,15 +156,15 @@ long double S4a_innerLoop(uint64_t x, uint64_t u, uint64_t l, long double psiOfU
   return result;
 }
 
-long double S4b(uint64_t x, uint64_t u, const long double psiOfU) {
+long double S4b(const uint64_t x, const long double u, const long double psiOfU) {
    long double sum = 0.0;
-   for (uint64_t l = 1; l <= u; ++l) {
+   for (uint64_t l = 1; l <= (uint64_t) u; ++l) { //floor was ok
       sum += mobius(l)*S4b_innerSum(x, u, l, psiOfU);
    }
    return sum;
 }
 
-long double S4b_innerSum(const uint64_t x , const uint64_t u, const uint64_t l, const long double psiOfU) {
+long double S4b_innerSum(const uint64_t x, const long double u, const uint64_t l, const long double psiOfU) {
    long double sum = 0.0;
    const long double end = std::sqrt(x/l); //k <= sqrt(x/l), k is an integer...
    for (uint64_t k = 1; k <= end; ++k) {
@@ -209,28 +210,24 @@ long long mobius_work(long long x) {
   return (k % 2 == 0) ? -1 : 1;
 }
 
-long double slowS4(uint64_t x, uint64_t u) {
+long double slowS4(const uint64_t x, const long double u) {
   long double psi_of_u = primetools::calculatePsiLongDouble(u);
   long double result = 0.0;
-  for(uint64_t l = 1; l <= u; ++l) {
-    // std::cout<<"initial Result = "<<result<<std::endl;
+  for(uint64_t l = 1; l <= (uint64_t) u; ++l) { //floor was ok
     long double sum = (mobius(l) * slowS4_inner(x, u, l, psi_of_u));
-    // std::cout<<"The sum is "<<sum<<std::endl;
     result += sum;
-    // std::cout<<"New result is "<<result<<std::endl<<std::endl;
-    // std::cout<<mobius(l) <<" * "<< slowS4_inner(x,u,l,psi_of_u);
-    // result += (mobius(l) * slowS4_inner(x, u, l, psi_of_u));
-    // std::cout<<"outer loop is "<<result<<std::endl;
   }
   if(result == 0)
     std::cout<<" final result for "<<x<<" is "<<result<<std::endl;
   return result;
 }
 
-long double slowS4_inner(uint64_t x, uint64_t u, uint64_t l, long double psiOfU) {
+
+//Wed June 27 3:49pm: No time to double check that u is genuinely treated as a long double for this function.
+long double slowS4_inner(const uint64_t x, const long double u, const uint64_t l, const long double psiOfU) {
   long double result = 0.0;
-  uint64_t uOverl = u/l;
-  uint64_t xOverul = x/(u * l);
+  uint64_t uOverl = u/((long double) l);
+  uint64_t xOverul = - static_cast<uint64_t>(  -   ( (long double)x)/(u * (long double)l)     );   //a less than NEED A CEILING Wikipedia: "Negating the argument switches floor and ceiling and changes the sign:"
   for(uint64_t m = uOverl + 1; m <= xOverul; ++m) {
     result += (primetools::calculatePsiLongDouble(x/(l * m)) - psiOfU);
     if(x == 63) {
